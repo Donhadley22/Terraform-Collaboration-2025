3bcbb42c858fc11b1214315cf90dabfd21513d74
Donhadley <donhadleygirlandchika@gmail.com>
2025-05-29 13:48:39 +0100

    backend.tf and .gitignore files added  provider.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "terraform-buck-2025"
     key    = "devops/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
     dynamodb_table = "terraform-lock-table"
   }
 }
