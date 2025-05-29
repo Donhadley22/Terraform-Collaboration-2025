dbd98efb081f52a957ab3686a5345a602f895319
Donhadley <donhadleygirlandchika@gmail.com>
2025-05-29 13:16:29 +0100

    modified backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,5 @@ terraform {
     key    = "devops/terraform.tfstate"
     region = "eu-central-1"
     dynamodb_table = "terraform-lock-table"
-    role_arn = "arn:aws:iam::067188631142:user/Donhadley"
   }
 }
