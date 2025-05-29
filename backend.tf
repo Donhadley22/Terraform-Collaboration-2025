0889e1dd51479eebef1c0fcb2a58c47074767c5b
Donhadley <donhadleygirlandchika@gmail.com>
2025-05-29 12:05:31 +0100

    modified backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-buck-2025 "
+    bucket = "terraform-buck-2025"
     key    = "devops/terraform.tfstate"
     region = "eu-central-1"
     dynamodb_table = "terraform-lock-table"
+    role_arn = "arn:aws:iam::067188631142:user/Donhadley"
   }
 }
